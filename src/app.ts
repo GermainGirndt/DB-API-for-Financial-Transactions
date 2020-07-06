84e3857141541b2537978590eeb2c07c152bcc71
GermainPereira <pereira.germain@outlook.com>
2020-07-06 07:11:50 -0300

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-
+import cors from 'cors';
 import routes from './routes';
 import AppError from './errors/AppError';
 
@@ -17,7 +17,7 @@ const app = express();
 app.use('/tmp', express.static(uploadConfig.directory));
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -23,7 +23,7 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
